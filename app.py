c2bd4a065bd485bd8a341c0de026a8c5b702155b
rudhatami <rud.hatami@gmail.com>
2025-03-20 12:50:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -389,10 +389,10 @@ if st.button("Predict"):
 
         col1, col2, col3 = st.columns(spec = [1, 2, 1])
 
-            with col2:
-                cm = sns.color_palette("coolwarm_r", as_cmap=True)  # Generate the color map
-                df_final = df_final.style.background_gradient(cmap=cm)  # Apply the gradient style
-                st.dataframe(df_final)  # Display the styled dataframe
+         with col2:
+             cm = sns.color_palette("coolwarm_r", as_cmap=True)  # Generate the color map
+             df_final = df_final.style.background_gradient(cmap=cm)  # Apply the gradient style
+             st.dataframe(df_final)  # Display the styled dataframe
 
 
     #     st.markdown("""
