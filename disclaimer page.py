7e76f87a4cb2c5015dbb240ab7d194d30e2c2f63
rudhatami <rud.hatami@gmail.com>
2025-03-20 10:42:21 +0000

    Create disclaimer page.py

---
+++ disclaimer page.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from PIL import Image
+import os
+
+
+st.write("DISCLAIMER")
